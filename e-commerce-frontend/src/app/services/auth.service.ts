cbbe19fb06c12905d95b9e25a90cd0d4d0ec2228
GiacomoCorti <giacomo.corti21@gmai.com>
2022-07-20 14:26:52 +0200

    Services added

--- e-commerce-frontend/src/app/services/auth.service.ts
+++ e-commerce-frontend/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
-import { Token } from './Token';
+import { Token } from '../models/Token';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
